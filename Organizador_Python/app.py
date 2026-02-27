930057c15bf45e9c56066a76022e2d6497a3f9fe
Miller M. Vasco <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-02-27 19:04:49 -0300

    ✨ feat: initial commit

---
+++ Organizador_Python/app.py
@@ -0,0 +1,24 @@
+import os
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title="Selecione uma pasta")
+
+lista_arquivos = os.listdir(caminho)
+
+locais = {
+    "imagens": [".png", ".jpg", ".jpeg"],
+    "planilhas": [".xlsx"],
+    "pdfs": [".pdf"],
+    "csv": [".csv"],
+    "icones": [".ico"],
+    "videos": [".mp4"],
+    "musicas": [".mp3"],
+}
+
+for arquivo in lista_arquivos:
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")    
